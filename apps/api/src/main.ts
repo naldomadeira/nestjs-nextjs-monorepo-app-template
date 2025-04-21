5db0ee77b462462c91567ed2a892641bd99d7398
github <naldomadeira@gmail.com>
2025-04-21 17:30:55 -0300

    chore: correct vercel deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,35 +1,31 @@
 import { AppModule } from '@/app.module';
 import { bootstrap } from '@/bootstrap';
 import { NestFactory } from '@nestjs/core';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
-import express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-// const main = async () => {
-//   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-//     bufferLogs: true,
-//   });
+const main = async () => {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
 
-//   await bootstrap(app);
-// };
-
-// main().catch((error) => {
-//   console.log(error);
-//   process.exit(1);
-// });
-
-const server = express();
-
-async function bootstrapApp() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(server),
-  );
   await bootstrap(app);
-  return server;
-}
+};
 
-// Exportação necessária para o Vercel
-module.exports = bootstrapApp();
+main().catch((error) => {
+  console.log(error);
+  process.exit(1);
+});
+
+// const server = express();
+
+// async function bootstrapApp() {
+//   const app = await NestFactory.create<NestExpressApplication>(
+//     AppModule,
+//     new ExpressAdapter(server),
+//   );
+//   await bootstrap(app);
+//   return server;
+// }
+
+// // Exportação necessária para o Vercel
+// module.exports = bootstrapApp();
